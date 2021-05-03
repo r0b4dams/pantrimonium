29b2fd1ad7f3c07965f98a6cf85a1dfb88f83c22
ricroc <rickrocero@gmail.com>
2021-05-03 14:09:23 -0700

    create db models and table associations

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,62 @@
+const User = require('./User');
+const Inventory = require('./Inventory');
+const Section = require('./Section');
+const Item = require('./Item');
+const Ingredient = require('./Ingredient');
+const Recipe = require('./Recipe');
+const Recipe_List = require('./Recipe_List');
+
+
+User.hasOne(Inventory, {
+   foreignKey: 'user_id',
+   onDelete: 'CASCADE'
+});
+
+Inventory.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Inventory.hasMany(Section, {
+    foreignKey: 'inventory_id'
+});
+
+Section.belongsTo(Inventory, {
+    foreignKey: 'inventory_id'
+});
+
+Section.hasMany(Item, {
+    foreignKey: 'section_id'
+});
+
+Item.belongsTo(Section, {
+    foreignKey: 'section_id'
+});
+
+User.hasOne(Recipe_List, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Recipe_List.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Recipe_List.hasMany(Recipe, {
+    foreignKey: 'recipe_list_id'
+});
+
+Recipe.belongsTo(Recipe_List, {
+    foreignKey: 'recipe_list_id'
+});
+
+Recipe.hasMany(Ingredient, {
+    foreignKey: 'recipe_id'
+});
+
+Ingredient.belongsTo(Recipe, {
+    foreignKey: 'recipe_id'
+});
+
+
+
+module.exports = { User, Inventory, Section, Item, Ingredient, Recipe, Recipe_List};
