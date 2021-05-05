e2b16b531f30fbd91a35e99a9ee05449d0250acb
Sung Kim <sungjk77@msn.com>
2021-05-05 11:32:45 -0700

    testing integration

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
+const {User, Item, Inventory} = require('../models');
+
 const router = require('express').Router();
 
 // authorization routes
@@ -11,6 +13,41 @@ router.use(auth);
 // test handlebars functionality
 router.get('/', function (req, res) {
     res.render('homepage');
+})
+
+// router.get('/test', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Item }],
+//       });
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('shopping', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+router.get('/shopping', function (req, res) {
+    if (!req.session.logged_in) {
+        res.render("homepage");
+    } else {
+        res.render('shopping');
+    }
+})
+
+router.get('/summary', function (req, res) {
+    if (!req.session.logged_in) {
+        res.render("homepage");
+    } else {
+        res.render('summary');
+    }
 })
 
 module.exports = router;
