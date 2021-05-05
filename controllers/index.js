ff041f5634a43f59932c72699e9f52cef8181f97
ricroc <rickrocero@gmail.com>
2021-05-05 11:33:44 -0700

    kitchen routing

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const User = require('./User');
+const Inventory = require('./Inventory');
+const Section = require('./Section');
+const Item = require('./Item');
 
 // authorization routes
 const auth = require("./auth");
@@ -12,5 +16,11 @@ router.use(auth);
 router.get('/', function (req, res) {
     res.render('homepage');
 })
+
+router.get('/kitchen/:id', async (req,res) => {
+    const sectionData = await Section.findByPk(req.params.id);
+    console.log(sectionData);
+    res.render('kitchen');
+});
 
 module.exports = router;
