bea76ba75bb0236914cd22a4ba6ae7dc0b0d2e98
Sung Kim <sungjk77@msn.com>
2021-05-05 14:21:12 -0700

    working on handlebars shopping list

--- controllers/index.js
+++ controllers/index.js
@@ -15,26 +15,37 @@ router.get('/', function (req, res) {
     res.render('homepage');
 })
 
-router.get('/test', async (req, res) => {
-    console.log(req.session)
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await Section.findBy(req.session.user.id, {
-        where: user_id = req.session.user.id,
-        // attributes: { exclude: ['password'] },
-        // include: [{ model: Section }],
-      });
-      console.log(userData);
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('shopping', {
-        ...Item
-      });
-    } catch (err) {
-      res.status(500).json(err);
+router.get('/test3', async function (req, res) {
+    if(!req.session.user) {
+        res.render('homepage')
+    } else {
+        const sectionData = await Section.findAll ({
+            where: {user_id: 1}
+        })
     }
-  });
+    console.log(sectionData);
+})
+
+
+// router.get('/test', async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await Section.findByPk(req.session.user.id, {
+//         where: {user_id : req.session.user.id},
+//         // attributes: { exclude: ['password'] },
+//         include: [{ model: Item }],
+//       });
+//       console.log(userData);
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('shopping', {
+//         ...Item
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 router.get('/shopping', function (req, res) {
     if (!req.session.user) {
