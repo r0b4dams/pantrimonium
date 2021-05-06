cf873d11131068491573f589628d96ce4593d9c9
ricroc <rickrocero@gmail.com>
2021-05-06 11:43:08 -0700

    backend item post put delete done

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,8 @@
 // localhost:3001/api
-
 const router = require('express').Router();
-
 const itemRoutes = require('./item-routes');
 
 
 router.use('/items', itemRoutes);
-
 
 module.exports = router;
--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const Item = require('../../models/User');
+const Item = require('../../models/Item');
 
-// localhost:3001/api/items
+
+// localhost:3001/api/items endpoint
 
 
 // create an item **NEEDS TO BE PUT IN A SPECIFIC SECTION**
-router.post('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const itemData = await Item.create({
       name: req.body.name,
@@ -14,7 +15,7 @@ router.post('/:id', async (req, res) => {
       unit_of_measurement: req.body.unit_of_measurement,
       par_level: req.body.par_level,
       exp_date: req.body.exp_date,
-      section_id: req.params.id
+      section_id: req.body.section_id
     });
     res.status(200.).json(itemData);
   } catch (err) {
@@ -24,7 +25,21 @@ router.post('/:id', async (req, res) => {
 
 // update an item by id
 router.put('/:id', async (req, res) => {
-
+  try {
+    const updatedItemData = await Item.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    
+    if(!updatedItemData) {
+      res.status(400).json({ message: 'No such item' })
+      return;
+    }
+    res.status(200).json(updatedItemData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // delete an item from a section by id
@@ -33,17 +48,18 @@ router.delete('/:id', async (req, res) => {
       const itemData = await Item.destroy({
         where: {
           id: req.params.id,
-          section_id = req.body.section_id
+          // section_id: req.body.section_id
         },
       });
   
       if (!itemData) {
-        res.status(404).json({ message: 'No category found with that id!' });
+        res.status(404).json({ message: 'No item found with that id!' });
         return;
       }
-      res.status(200).json(categoryData);
-  
+      res.status(200).json(itemData);
     } catch (err) {
       res.status(500).json(err);
     }
 });
+
+module.exports = router;
