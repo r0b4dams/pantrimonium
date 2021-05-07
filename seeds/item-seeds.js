a31a22b3279b3a7ca2b59c9112868f15325d00f9
Robert Adams <adamsiii.robert@gmail.com>
2021-05-07 05:40:57 -0700

    modified seed data

--- seeds/item-seeds.js
+++ seeds/item-seeds.js
@@ -4,34 +4,35 @@ const itemData = [
     {
       name: "milk",
       type: "dairy",
-      quantity: 1,
+      quantity: 2,
       unit_of_measurement: "gallon",
       par_level: 1,
-      exp_date: "2021-05-05",
+      exp_date: "2021-05-01",
       section_id: 1
     },
     {
-      name: "eggs",
+      name: "egg",
       type: "dairy",
-      quantity: 12,
-      par_level: 3,
+      quantity: 1,
+      unit_of_measurement: "dozen",
+      par_level: 1,
+      exp_date: "2021-05-28",
       section_id: 1
     },
     {
       name: "ice cream",
       type: "dairy",
       quantity: 1,
-      unit_of_measurement: "total",
+      unit_of_measurement: "gallon",
       par_level: 1,
-      exp_date: "2021-06-28",
       section_id: 2
     },
     {
-      name: "cheddar cheese",
-      type: "dairy",
-      quantity: 2,
+      name: "frozen peas",
+      type: "vegetable",
+      quantity: 1,
+      unit_of_measurement: "bag",
       par_level: 1,
-      exp_date: "2021-07-28",
       section_id: 2
     },
     {
@@ -40,16 +41,14 @@ const itemData = [
       quantity: 1,
       unit_of_measurement: "loaf",
       par_level: 1,
-      exp_date: "2021-05-05",
       section_id: 3
     },
     {
-      name: "mixed vegetables",
-      type: "vegetables",
-      quantity: 2,
-      unit_of_measurement: "bag",
+      name: "peanut butter",
+      type: "nut",
+      quantity: 1,
+      unit_of_measurement: "jar",
       par_level: 1,
-      exp_date: "2021-05-05",
       section_id: 3
     },
     {
@@ -58,9 +57,148 @@ const itemData = [
       quantity: 0.5,
       unit_of_measurement: "gallon",
       par_level: 1,
-      exp_date: "2021-05-05",
+      section_id: 1
+    },
+    {
+      name: "bbq sauce",
+      type: "condiment",
+      quantity: 1,
+      unit_of_measurement: "bottle",
+      par_level: 1,
+      exp_date: "2021-06-04",
+      section_id: 1
+    },
+    {
+      name: "frozen burger patties",
+      type: "meat",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      exp_date: "2021-08-04",
       section_id: 2
     },
+    {
+      name: "ice cream",
+      type: "dairy",
+      quantity: 1,
+      unit_of_measurement: "gallon",
+      par_level: 1,
+      section_id: 2
+    },
+    {
+      name: "oreos",
+      type: "snack",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 3
+    },
+    {
+      name: "goldfish",
+      type: "snack",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 3
+    },
+    {
+      name: "deli sliced turkey",
+      type: "meat",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 1
+    },
+    {
+      name: "pepperjack cheese slices",
+      type: "dairy",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      exp_date: "2021-05-28",
+      section_id: 1
+    },
+    {
+      name: "frozen corn",
+      type: "vegetable",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 2
+    },
+    {
+      name: "frozen broccoli",
+      type: "vegetable",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 2
+    },
+    {
+      name: "sour cream & onion chips",
+      type: "snack",
+      quantity: 1,
+      unit_of_measurement: "bag",
+      par_level: 1,
+      section_id: 2
+    },
+    {
+      name: "rice crackers",
+      type: "snack",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      section_id: 2
+    },
+    {
+      name: "kimchi",
+      type: "vegetable",
+      quantity: 1,
+      unit_of_measurement: "jar",
+      par_level: 1,
+      section_id: 1
+    },
+    {
+      name: "strawberries",
+      type: "fruit",
+      quantity: 1,
+      unit_of_measurement: "package",
+      par_level: 1,
+      exp_date: "2021-05-11",
+      section_id: 1
+    },
+    {
+      name: "keto breakfast sandwich",
+      type: "meat",
+      quantity: 1,
+      unit_of_measurement: "box",
+      par_level: 1,
+      section_id: 1
+    },
+    {
+      name: "orange chicken",
+      type: "meat",
+      quantity: 1,
+      unit_of_measurement: "box",
+      par_level: 1,
+      section_id: 1
+    },
+    {
+      name: "onion",
+      type: "vegetable",
+      quantity: 3,
+      unit_of_measurement: "bulb",
+      par_level: 1,
+      section_id: 1
+    },
+    {
+      name: "croutons",
+      type: "grain",
+      quantity: 1,
+      unit_of_measurement: "bag",
+      par_level: 1,
+      section_id: 3
+    }, 
 ]
 
 const seedItems = () => Item.bulkCreate(itemData);
