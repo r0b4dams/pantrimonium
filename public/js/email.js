2d44dd971eeab17cba3de3fd5af233486b6152b5
Robert Adams <adamsiii.robert@gmail.com>
2021-05-08 15:15:06 -0700

    removed console log

--- public/js/email.js
+++ public/js/email.js
@@ -1,9 +1,8 @@
-
 const emailBtn = document.querySelector('#emailBtn');
 
 // request server to send shopping list to user via email
 emailBtn.addEventListener('click', async function() {
-    console.log("button clicked");
+
     const response = await fetch('/auth/email', {
         method: 'POST',
       });
